7b05a63b5576fc867abc641143e7526a3658b511
Sami Kilpinen <sami.kilpinen@helsinki.fi>
2022-12-09 12:12:35 +0200

    Additional functions file

---
+++ code/AdditionalFunctions.R
@@ -0,0 +1,83 @@
+
+gene_set_prepare2 <- function(path_to_db_file, cell_type){
+  
+  cell_markers = openxlsx::read.xlsx(path_to_db_file)
+  cell_markers = cell_markers[cell_markers$tissueType == cell_type,] 
+  cell_markers$geneSymbolmore1 = gsub(" ","",cell_markers$geneSymbolmore1); cell_markers$geneSymbolmore2 = gsub(" ","",cell_markers$geneSymbolmore2)
+  
+  # correct gene symbols from the given DB (up-genes)
+  cell_markers$geneSymbolmore1 = sapply(1:nrow(cell_markers), function(i){
+    
+    markers_all = gsub(" ", "", unlist(strsplit(cell_markers$geneSymbolmore1[i],",")))
+    markers_all = toupper(markers_all[markers_all != "NA" & markers_all != ""])
+    markers_all = sort(markers_all)
+    paste0(markers_all, collapse=",")
+  })
+  
+  # correct gene symbols from the given DB (down-genes)
+  cell_markers$geneSymbolmore2 = sapply(1:nrow(cell_markers), function(i){
+    
+    markers_all = gsub(" ", "", unlist(strsplit(cell_markers$geneSymbolmore2[i],",")))
+    markers_all = toupper(markers_all[markers_all != "NA" & markers_all != ""])
+    markers_all = sort(markers_all)
+    paste0(markers_all, collapse=",")
+  })
+  
+  cell_markers$geneSymbolmore1 = gsub("///",",",cell_markers$geneSymbolmore1);cell_markers$geneSymbolmore1 = gsub(" ","",cell_markers$geneSymbolmore1)
+  cell_markers$geneSymbolmore2 = gsub("///",",",cell_markers$geneSymbolmore2);cell_markers$geneSymbolmore2 = gsub(" ","",cell_markers$geneSymbolmore2)
+  
+  gs = lapply(1:nrow(cell_markers), function(j) gsub(" ","",unlist(strsplit(toString(cell_markers$geneSymbolmore1[j]),",")))); names(gs) = cell_markers$cellName
+  gs2 = lapply(1:nrow(cell_markers), function(j) gsub(" ","",unlist(strsplit(toString(cell_markers$geneSymbolmore2[j]),",")))); names(gs2) = cell_markers$cellName
+  
+  list(gs_positive = gs, gs_negative = gs2)
+}
+
+convert_feature_identity <- function(object, assay, features, feature.format = "symbol") {
+  
+  #'
+  #' Converts ENS ID -> gene symbol and vice versa
+  #' Returns a vector of length(features) of either matches or NAs, in corresponding indices.
+  #'
+  #' Assumes libraries dplyr and seuratObject.
+  #' Moreover, requires seuratObject[["assay"]] to contain df/tbl of ENS-symbol correspondences.
+  #'
+  # Protective tests
+  if (!any(feature.format %in% c("ens", "symbol"))) {
+    stop("Feature format should be a sting: either 'symbol' or 'ens'")
+  }
+  if (length(features) == 0) {
+    stop("No features found in argument 'features'")
+  }
+  if (feature.format == "ens" & !all(grepl("*ENS", features))) {
+    message("Warning: Found non-ENS ID for argument feature format 'ens'")
+  }
+  if (feature.format == "symbol" & any(grepl("*ENS", features))) {
+    message("Warning: Found ENS ID for argument feature format 'symbol'")
+  }
+  # Diverging execution: case if provided features are ENSEBL IDs => conversion to symbols
+  if (feature.format == "ens") {
+    
+    object.features <- object[[assay]][[]] %>%
+      rownames_to_column(var = "gene_id") %>%
+      as_tibble() %>%
+      dplyr::select("gene_id", "feature_symbol") %>%
+      dplyr::filter(gene_id %in% features)
+    match.index <- match(features, object.features$gene_id, nomatch = NA)
+    v.out <- sapply(match.index, function (i) { ifelse(is.na(i), NA, object.features$feature_symbol[i])})
+    
+    sprintf("Instance: Found matching for %d features out of total %d provided features", sum(!is.na(v.out)), length(features)) %>%
+      print()
+    
+    return (v.out)
+}}
+  
+create_dt <- function(x){
+    DT::datatable(x,
+                  extensions = 'Buttons',
+                  options = list(dom = 'Blfrtip',
+                                 buttons = c('copy', 'csv', 'excel', 'pdf'),
+                                 lengthMenu = list(c(10,25,50,-1),
+                                                   c(10,25,50,"All"))))
+}
+  
+  
